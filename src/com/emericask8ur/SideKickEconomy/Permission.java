57da6c207e01f8b3392ed1bb7bc83c53cf789411
emericask8ur <sk8tj@yahoo.com>
2012-06-08 15:26:53 -0400

    Added new boolean method under permissions
    Signed-off-by: emericask8ur <sk8tj@yahoo.com>

--- src/com/emericask8ur/SideKickEconomy/Permission.java
+++ src/com/emericask8ur/SideKickEconomy/Permission.java
@@ -20,5 +20,21 @@ public class Permission {
 			return false;
 		}
 	}
+	
+	/**
+	 * Checks and returns if a player has permission to use a node.</br>
+	 * If not, the player will return a message "You do not have permission".
+	 * @param player
+	 * @param node
+	 * @return
+	 */
+	public static boolean hasPermission(Player player, String node){
+		if(player.hasPermission(node)){
+			return true;
+		} else {
+			player.sendMessage(ChatColor.RED + "You do not have permission");
+			return false;
+		}
+	}
 
 }
